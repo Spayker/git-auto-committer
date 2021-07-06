b5aa5bc64247618e5f69b633b7e12c89dd1552ce
spayker <spykerstar@gmail.com>
2021-07-06 23:35:30 +0200

    updated folder scanning

--- java_based/src/main/java/com/spayker/ac/Main.java
+++ java_based/src/main/java/com/spayker/ac/Main.java
@@ -1,13 +1,24 @@
 package com.spayker.ac;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
 
+    private static final String CURRENT_APP_RUNNING_DIR = System.getProperty("user.dir");
 
+    public static void main(String[] args) {
 
+        System.out.println("Working Directory = " + CURRENT_APP_RUNNING_DIR);
 
+        // get folder projects
+        File directory = new File(CURRENT_APP_RUNNING_DIR);
+        File[] projectFolders = directory.listFiles(File::isDirectory);
 
-
-
-
+        System.out.println("Found project folders:");
+        Arrays.stream(Objects.requireNonNull(projectFolders)).forEach(System.out::println);
+    }
 
 }
