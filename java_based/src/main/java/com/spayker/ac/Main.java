5fd3406712a075357dd28a1177b13ab0da9cbeca
spayker <spykerstar@gmail.com>
2021-07-19 23:37:05 +0200

    modified java_based/src/main/java/com/spayker/ac/Main.java

--- java_based/src/main/java/com/spayker/ac/Main.java
+++ java_based/src/main/java/com/spayker/ac/Main.java
@@ -2,24 +2,9 @@ package com.spayker.ac;
 
 import com.spayker.ac.task.ChangeProcessor;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.Status;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.StoredConfig;
-import org.eclipse.jgit.lib.UserConfig;
-import org.eclipse.jgit.transport.CredentialsProvider;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
-import static com.spayker.ac.model.git.CHANGE.*;
 
 @Slf4j
 public class Main {
@@ -29,7 +14,7 @@ public class Main {
     private static final int POOL_SIZE = 1;
     private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(POOL_SIZE);
 
-    private static final long INITIAL_DELAY = 0;
+    private static final long INITIAL_DELAY = 1;
     private static final long PERIOD = 15;
     private static final TimeUnit UNIT = TimeUnit.MINUTES;
 
