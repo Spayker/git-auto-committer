cc7d726af2cfb9f833527f5135f0ae3a2fd75672
spayker <spykerstar@gmail.com>
2021-07-24 14:38:33 +0200

    modified java_based/src/main/java/com/spayker/ac/model/git/AppConfig.java

--- java_based/src/main/java/com/spayker/ac/model/git/AppConfig.java
+++ java_based/src/main/java/com/spayker/ac/model/git/AppConfig.java
@@ -2,8 +2,13 @@ package com.spayker.ac.model.git;
 
 public class AppConfig {
 
+    private String token;
     private int runPeriod;
     private int initialDelay;
+
+    public String getToken() {
+        return token;
+    }
 
     public int getRunPeriod() {
         return runPeriod;
