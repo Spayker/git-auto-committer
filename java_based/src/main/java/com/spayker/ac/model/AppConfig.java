167a5334e5ddf6929be33a22465d0c81727bba97
spayker <spykerstar@gmail.com>
2021-07-24 14:56:11 +0200

    added java_based/src/main/java/com/spayker/ac/model/AppConfig.java

--- java_based/src/main/java/com/spayker/ac/model/AppConfig.java
+++ java_based/src/main/java/com/spayker/ac/model/AppConfig.java
@@ -1,20 +1,12 @@
-package com.spayker.ac.model.git;
+package com.spayker.ac.model;
+
+import lombok.Getter;
 
 public class AppConfig {
-
+    @Getter
     private String token;
+    @Getter
     private int runPeriod;
+    @Getter
     private int initialDelay;
-
-    public String getToken() {
-        return token;
-    }
-
-    public int getRunPeriod() {
-        return runPeriod;
-    }
-
-    public int getInitialDelay() {
-        return initialDelay;
-    }
 }
